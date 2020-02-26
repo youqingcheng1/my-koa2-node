f2ea002955e25fe3d0b283cd4112a5fc8e3b1a4c
youqincheng <youqincheng@mini1.cn>
2020-02-26 19:00:13 +0800

    链接数据库，请求数据

---
+++ controllers/defaultConfig.js
@@ -0,0 +1,13 @@
+//defaultConfig.js
+const config = {
+    // 数据库配置
+    database: {
+        DATABASE: 'test', //数据库名称
+        USERNAME: 'root', //mysql用户名
+        PASSWORD: 'yqc123456', //mysql密码
+        PORT: '3306', //mysql端口号
+        HOST: 'localhost' //服务器ip
+    }
+}
+
+module.exports = config
---
+++ routes/users.js
@@ -0,0 +1,36 @@
+const router = require('koa-router')()
+const userService = require('../controllers/mysqlConfig');
+
+router.prefix('/users')
+
+//获取所有用户(GET请求)
+router.get('/', async (ctx, next) => {
+  // console.log(ctx.session.username)
+  ctx.body = await userService.findUserData('ddsd');
+})
+
+// 增加用户(POST请求)
+router.post('/add', async (ctx, next) => {
+  let arr = [];
+
+  arr.push(ctx.request.body['name']);
+  arr.push(ctx.request.body['pass']);
+  arr.push(ctx.request.body['auth']);
+
+  await userService.addUserData(arr)
+      .then((data) => {
+          let r = '';
+          if (data.affectedRows != 0) {
+              r = 'ok';
+          }
+          ctx.body = {
+              data: r
+          }
+      }).catch(() => {
+          ctx.body = {
+              data: 'err'
+          }
+      })
+})
+
+module.exports = router
